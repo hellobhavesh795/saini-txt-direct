41140d7357089aa021702ad4f13cc1d2cd7040f5
hellobhavesh795 <mrbhavwsh09@gmail.com>
2025-09-06 16:29:53 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -3,11 +3,11 @@
 import os
 from os import environ
 
-API_ID = int(environ.get("API_ID", "22182189"))
-API_HASH = environ.get("API_HASH", "5e7c4088f8e23d0ab61e29ae11960bf5")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
+API_ID = int(environ.get("API_ID", "21866171"))
+API_HASH = environ.get("API_HASH", "5788dba8f23fade5edda55948e985f06")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8404900053:AAGItqL5TH6t241wUa0Dt03n3yj_MaBE4tU")
 
-OWNER = int(environ.get("OWNER", "5680454765"))
+OWNER = int(environ.get("OWNER", "6927406670"))
 CREDIT = environ.get("CREDIT", "𝙎𝘼𝙄𝙉𝙄 𝘽𝙊𝙏𝙎")
 cookies_file_path = os.getenv("cookies_file_path", "youtube_cookies.txt")
 
@@ -33,3 +33,4 @@ photoyt = 'https://tinypic.host/images/2025/03/18/YouTube-Logo.wine.png' #https:
 photocp = 'https://tinypic.host/images/2025/03/28/IMG_20250328_133126.jpg'
 photozip = 'https://envs.sh/cD_.jpg'
 # .....,.....,.......,...,.......,....., .....,.....,.......,...,.
+
